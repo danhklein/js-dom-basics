9730f2d5ddb1f10b19bbc45d293c6d03defd46ea
Daniel Klein <danhirschklein@gmail.com>
2016-01-14 15:17:24 -0700

    Added list of basic example commands

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log(sanity check)
+// var getAllElements = document.getElementsByTagName('ul');
+// console.log (getAllElements)
+
+// var getSingleElement = document.getElementsByTagName('ul')[0];  
+// console.log(getSingleElement)
+
+// var firstParagraph = document.getElementById('main');
+// console.log(firstParagraph);
+
+// var allListItems = document.getElementsByTagName('li');
+// console.log(allListItems)
+
+// for (var i = 0; i < allListItems.length; i++) {
+//   console.log(allListItems[i]);
+// }
+
+// var evenElements = document.getElementsByClassName('even');
+// console.log(evenElements)
+
+// var oddElements = document.getElementsByClassName('odd');
+// console.log(oddElements)
+
+// var firstEvenElement = document.getElementsByClassName('even')[0];
+// console.log(firstEvenElement)
+
+// var firstOddElement = document.getElementsByClassName('odd')[0];
+// console.log(firstOddElement)
+
+// var main = document.querySelector('#main');
+// console.log(main);
+
+// var firstOddElement = document.querySelector('.odd');
+// console.log(firstOddElement);
+
+var oddAndEvenElements = document.querySelectorAll('.odd, .even');
+console.log(oddAndEvenElements);
